27e422586c0e16d11cb589b1a883ad5ccff78a97
Alex Ralph <alexanderralph@hastingsdirect.com>
2025-12-03 14:37:40 +0000

    optimise day 2 slightly

--- day02.py
+++ day02.py
@@ -8,10 +8,12 @@ def parse_data():
 # Disclaimer: I only realised this could be regexed after it was pointed out to me
 # This is not the first solution I used
 def sum_invalid_range_p1(start: str, end: str) -> int:
-    return sum(x for x in range(int(start), int(end)+1) if re.fullmatch(r"([0-9]+)\1", str(x)))
+    regex = re.compile(r"([0-9]+)\1")
+    return sum(x for x in range(int(start), int(end)+1) if regex.fullmatch(str(x)))
 
 def sum_invalid_range_p2(start: str, end: str) -> int:
-    return sum(x for x in range(int(start), int(end)+1) if re.fullmatch(r"([0-9]+)\1+", str(x)))
+    regex = re.compile(r"([0-9]+)\1+")
+    return sum(x for x in range(int(start), int(end)+1) if regex.fullmatch(str(x)))
 
 print(f"Part one: {sum(sum_invalid_range_p1(x[0], x[1]) for x in parse_data())}")
 print(f"Part two: {sum(sum_invalid_range_p2(x[0], x[1]) for x in parse_data())}")
