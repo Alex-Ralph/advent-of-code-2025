e47730f107dc8472032653ab0ac0840b28f25ae5
Alex Ralph <alexanderralph@hastingsdirect.com>
2025-12-05 15:13:50 +0000

    refactor day 5

--- day05.py
+++ day05.py
@@ -1,36 +1,29 @@
-
-print("starting")
 with open("input/day05.txt") as file:
     rangelist, ingredients_list = [x.splitlines() for x in file.read().split("\n\n")]
 
-fresh_id_ranges = [(int(x[0]), int(x[1])) for x in [y.split("-") for y in rangelist]]
+fresh_id_ranges = [[int(x[0]), int(x[1])] for x in [y.split("-") for y in rangelist]]
 ingredient_ids = (int(x) for x in ingredients_list)
 
 sorted_id_ranges = sorted(fresh_id_ranges, key=lambda x: x[0])
 
-def reduce_id_ranges(id_ranges: list[tuple[int]]) -> list[tuple[int]]:
-    if len(id_ranges) == 0:
-        return []
-    highest_in_first_range = id_ranges[0][1]
-    reduced_list = []
-    for id_range in id_ranges[1:]:
-        if id_range[1] <= highest_in_first_range:
-            continue
-        if id_range[0] <= highest_in_first_range:
-            reduced_list.append((highest_in_first_range+1, id_range[1]))
-        reduced_list.append(id_range)
-    return [(id_ranges[0])] + [x for x in reduce_id_ranges(reduced_list)]
+def remove_overlap(id_ranges: list[tuple[int]]) -> list[tuple[int]]:
+    for index, id_range in enumerate(id_ranges[:-1]):
+        if id_range[1] > id_ranges[index+1][1]:
+            id_ranges[index+1][1] = id_range[1]
+        if id_range[1] >= id_ranges[index+1][0]:
+            id_range[1] = id_ranges[index+1][0] - 1
+    return [x for x in id_ranges if x[0] <= x[1]]
 
-reduced_fresh_id_ranges = reduce_id_ranges(sorted_id_ranges)
+no_overlap_ranges = remove_overlap(sorted_id_ranges)
 
-fresh_ingredients = [
-    x for x in ingredient_ids
-    if any(x >= id_range[0] and x <= id_range[1] for id_range in reduced_fresh_id_ranges)
-]
+fresh_ingredient_count = sum([
+    1 for x in ingredient_ids
+    if any(x >= id_range[0] and x <= id_range[1] for id_range in no_overlap_ranges)
+])
 
-fresh_ingredient_count = sum(
+fresh_ingredient_ids = sum(
     (id_range[1] - id_range[0]) + 1 
-    for id_range in reduced_fresh_id_ranges)
+    for id_range in no_overlap_ranges)
 
-print(len(fresh_ingredients))
 print(fresh_ingredient_count)
+print(fresh_ingredient_ids)
